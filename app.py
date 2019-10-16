d439c0945caa489c3010ddff53094165c582c57a
Arash <arashfarahani@hotmail.com>
2019-10-16 23:03:13 +0330

    custom data stored and fetched very well

--- app.py
+++ app.py
@@ -5,30 +5,28 @@ from bson.json_util import dumps
 import json
 
 client = MongoClient('localhost:27017')
-db = client.ContactDB
+db = client.DataDB
 
 app = Flask(__name__)
 
-@app.route("/add_contact", methods = ['POST'])
-def add_contact():
+@app.route("/add", methods = ['POST'])
+def add():
     try:
         data = json.loads(request.data)
-        user_name = data['name']
-        user_contact = data['contact']
-        if user_name and user_contact:
-            status = db.Contacts.insert_one({
-                "name" : user_name,
-                "contact" : user_contact
-            })
+        print(data)
+        
+        if(data):
+            db.Data.insert_one(data);
+
         return dumps({'message' : 'SUCCESS'})
     except Exception as e:
         return dumps({'error' : str(e)})
 
-@app.route("/get_all_contact", methods = ['GET'])
-def get_all_contact():
+@app.route("/getAll", methods = ['GET'])
+def getAll():
     try:
-        contacts = db.Contacts.find()
-        return dumps(contacts)
+        data = db.Data.find()
+        return dumps(data)
     except Exception as e:
         return dumps({'error' : str(e)})
 
